8dd69f5903c271a9843da0bfe5ac7df671a54c73
tavelars <tavelars@gmail.com>
2023-01-31 21:53:46 -0300

    Componentizando a passagem de parametro via props

--- fundamentos-react/src/index.js
+++ fundamentos-react/src/index.js
@@ -3,14 +3,17 @@ import React from 'react'
 import './index.css'
 
 import Primeiro from './components/basicos/Primeiro'
+import ComParametro from './components/basicos/ComParametro'
 
 const nome = <strong>Thiago J A Silva</strong>
 
 ReactDOM.render(
-        <div>
-            <strong>Olá React,</strong> {nome} 
-            <br></br>
-            <Primeiro></Primeiro>
-        </div>, 
+    <div>
+        <strong>Olá React,</strong> {nome}
+        <br />
+        <Primeiro></Primeiro>
+        <ComParametro titulo="Segundo componente desenvolvido" subtitulo="React componentizando"></ComParametro>
+        <ComParametro titulo="Customizando mensagens via parametro" subtitulo="Tudo parametrizado"></ComParametro>
+    </div>,
     document.getElementById("root")
 )
