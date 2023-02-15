d81f558aaa8b5f49eb16f2e35c0e803231123488
tavelars <tavelars@gmail.com>
2023-02-15 19:48:41 -0300

    Congiguração do estilo e onchange de qtde

--- fundamentos-react/src/components/mega/Mega.jsx
+++ fundamentos-react/src/components/mega/Mega.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import './Mega.css'
 
 export default props => {
 
@@ -14,28 +15,33 @@ export default props => {
                 const novoNumero = gerarNumeroNaoContido(1, 60, nums)
                 return [...nums, novoNumero]
             }, [])
-            .sort((n1, n2) => n1- n2)
+            .sort((n1, n2) => n1 - n2)
         return numeros
     }
 
     const [qtde, setQtde] = useState(props.qtde || 6)
     const numerosIniciais = Array(props.qtde || 6).fill(0)
     const [numeros, setNumeros] = useState(numerosIniciais)
-    
+
 
     return (
-        <div>
+        <div className="Mega">
             <h3>Mega Sena</h3>
-            
+
             <h2>{numeros.join(' ')}</h2>
-            
+
             <div>
                 <label>Qtde Números</label>
-                <input type="number" value={qtde} onChange={e => setQtde(+e.target.value)}/>
+                <input type="number" value={qtde}
+                    min="6" max="9"
+                    onChange={e => {
+                        setQtde(+e.target.value)
+                        setNumeros(gerarNumeros(+e.target.value))
+                    }} />
             </div>
 
             <button onClick={_ => setNumeros(gerarNumeros(qtde))}>Gerar Números</button>
-                
+
         </div>
     )
 }
